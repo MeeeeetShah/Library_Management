0e57cf23ad91a4fcbbc9900488255af70f917b14
Meeeeeetshah <shahmeetjayendrakumar@gmail.com>
2024-12-28 19:44:38 +0530

    adding 1st tets to add book into a library

---
+++ __tests__/library.test.js
@@ -0,0 +1,8 @@
+const Library = require('./library');
+
+test('should add a book to the library', () => {
+  const library = new Library();
+  const book = { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', isbn: '1234567890' };
+  library.addBook(book);
+  expect(library.getBooks()).toContain(book);
+});
