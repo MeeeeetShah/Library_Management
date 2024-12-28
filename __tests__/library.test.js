4ee70c17edfcf3b739aa5c5d665174bea96e6a41
Meeeeeetshah <shahmeetjayendrakumar@gmail.com>
2024-12-28 19:56:10 +0530

    Test 2 -- Borrow book (by ISBN simple case)

--- __tests__/library.test.js
+++ __tests__/library.test.js
@@ -4,5 +4,6 @@ test('should add a book to the library', () => {
   const library = new Library();
   const book = { title: 'The Great Gatsby', author: 'Meet Shah', isbn: '1234567890' };
   library.addBook(book);
+  library.borrowBook('1234567890');
   expect(library.getBooks()).toContain(book);
 });
